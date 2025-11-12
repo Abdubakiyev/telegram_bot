1528b12e46c36aba56be55271914d36b6e251de1
Abdulloh Abduboqiyev <abduboqiyevshohruxbek@gmail.com>
2025-11-12 09:46:16 +0300

    first commit 10

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000; // Render PORT
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
